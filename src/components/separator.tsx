39bd47c7e9aaad77299e8ac610fdbfb27e4c1e81
Andy <andy@gettimesaved.com>
2020-07-16 02:36:22 -0400

    Add separator component

---
+++ src/components/separator.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Separator() {
+  return (
+    <div style={{ width: "100%", textAlign: "center", marginBottom: 24 }}>
+      •••
+    </div>
+  )
+}
